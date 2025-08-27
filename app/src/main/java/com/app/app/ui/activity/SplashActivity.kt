2b0db1c30883657d47fd0fc6166e6a0877ceab73
Jinal Nabhoya <jinal.nabhoya.helios@gmail.com>
2025-08-27 18:02:32 +0530

    init

---
+++ app/src/main/java/com/app/app/ui/activity/SplashActivity.kt
@@ -0,0 +1,24 @@
+package com.app.app.ui.activity
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.app.app.R
+import com.app.app.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
---
+++ app/src/main/java/com/app/app/ui/base/BaseActivity.kt
@@ -0,0 +1,6 @@
+package com.app.app.ui.base
+
+import androidx.appcompat.app.AppCompatActivity
+
+open class BaseActivity : AppCompatActivity() {
+}
